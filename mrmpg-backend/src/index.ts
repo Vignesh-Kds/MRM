5038436badc6fc4051a542f9b8158147cb86151a
root <root@ip-172-31-8-38.ap-south-1.compute.internal>
2026-03-30 12:11:25 +0000

    fix:bind server for k8s

--- mrmpg-backend/src/index.ts
+++ mrmpg-backend/src/index.ts
@@ -1,6 +1,6 @@
 import app from "./app";
 import { ENV } from "./config/env";
 
-app.listen(ENV.PORT, () => {
-  console.log(`Server running on http://localhost:${ENV.PORT} in ${ENV.NODE_ENV} mode`);
+app.listen(ENV.PORT, "0.0.0.0", () => {
+  console.log(`Server running on http://0.0.0.0:${ENV.PORT} in ${ENV.NODE_ENV} mode`);
 });
